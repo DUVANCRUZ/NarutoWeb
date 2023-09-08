c88e7f21d1328971ee5b119661a1d5d24ba2db1f
duvandres9820@gmail.com <duvandres9820@gmail.com>
2023-09-09 01:22:15 +0200

    deploy 12

--- back end/src/app.py
+++ back end/src/app.py
@@ -2,15 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.character_routes import char_routes
 
-
 app = Flask(__name__)
 
 # Configurar la conexión con MongoDB utilizando la función initialize_mongo
 CORS(app)
 
 # Registrar los Blueprints de las rutas
-app.register_blueprint(char_routes)# Registra el blueprint de personajes
-
+app.register_blueprint(char_routes)  # Registra el blueprint de personajes
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Cambiar el puerto a 8080
+    app.run(host='0.0.0.0', port=8080, debug=True)
